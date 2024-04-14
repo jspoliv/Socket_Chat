2ab492290472aecb4de462c6ce0df74026dab1ed
jspol <jspoliv@gmail.com>
2024-04-14 17:06:56 -0300

    client cleanup

--- Client.c
+++ Client.c
@@ -3,26 +3,15 @@
 #include <winsock2.h>
 #include <pthread.h>
 
-SOCKET createTCPIPv4Socket() {
-    return socket(AF_INET, SOCK_STREAM, 0);
-}
+// IPv4 TCP socket
+SOCKET newSocket();
 
-SOCKADDR_IN* createIPv4Address(char *ip, int port) {
-    SOCKADDR_IN *address = malloc(sizeof(SOCKADDR_IN));
-    if (address == NULL)
-        return NULL;
-    address->sin_family = AF_INET;
-    address->sin_port = htons(port);
+// IPv4 address
+SOCKADDR_IN newAddress(char *ip, int port);
 
-    if (strlen(ip) == 0)
-        address->sin_addr.s_addr = INADDR_ANY;
-    else
-        address->sin_addr.s_addr = inet_addr(ip);
+void* sendLoop(SOCKET* socket);
 
-    return address;
-}
-
-void* recvPrint(void* socketFD) {
+void* recvLoop(void* socketFD) {
     SOCKET* socket = (SOCKET*)socketFD;
     char buffer[1024];
     while(1) {
@@ -39,51 +28,69 @@ void* recvPrint(void* socketFD) {
     }
 }
 
-void recvPrintThread(SOCKET *socketFD) {
-    pthread_t id;
-    pthread_create(&id, NULL, recvPrint, socketFD);
-}
-
 int main() {
     WSADATA WSAData;
-    WSAStartup(MAKEWORD(2, 0), &WSAData);
+    SOCKET client_socket;
+    SOCKADDR_IN server_addr;
+    pthread_t id;
 
-    SOCKET socketFD = createTCPIPv4Socket();
-    if (socketFD == INVALID_SOCKET) {
+    if (WSAStartup(MAKEWORD(2, 0), &WSAData) != 0) {
+        printf("[WSAStartup() failed]\n\n");
+        system("pause");
+        exit(1);
+    }
+    printf("[WSAStartup() resolved successfully]\n");
+
+    client_socket = newSocket();
+    if (client_socket == INVALID_SOCKET) {
         printf("[socket() failed]\n\n");
         WSACleanup();
         system("pause");
-        return -1;
+        exit(1);
     }
     printf("[socket() resolved successfully]\n");
 
-    SOCKADDR_IN *address = createIPv4Address("127.0.0.1", 3000);
-    if (address == NULL) {
-        printf("[createIPv4Address() failed]\n\n");
-        closesocket(socketFD);
+    server_addr = newAddress("127.0.0.1", 3000);
+
+    if (connect(client_socket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        printf("[connect() failed]\n\n");
+        closesocket(client_socket);
         WSACleanup();
         system("pause");
-        return -1;
+        exit(1);
     }
-    printf("[createIPv4Address() resolved successfully]\n");
+    printf("[connect() resolved successfully]\n");
 
-    int connect_result = connect(socketFD, (SOCKADDR *)address, sizeof(*address));
-    if (connect_result == SOCKET_ERROR) {
-        printf("[Connection failed]\n\n");
-        closesocket(socketFD);
-        free(address);
-        WSACleanup();
-        system("pause");
-        return -1;
-    }
-    printf("[Connected to server]\n");
+    pthread_create(&id, NULL, recvLoop, &client_socket);
 
+    sendLoop(&client_socket);
+
+    closesocket(client_socket);
+    WSACleanup();
+    printf("\n");
+    system("pause");
+    return 0;
+}
+
+SOCKET newSocket() {
+    return socket(AF_INET, SOCK_STREAM, 0);
+}
+
+SOCKADDR_IN newAddress(char *ip, int port) {
+    SOCKADDR_IN addr_in;
+    addr_in.sin_family = AF_INET;
+    addr_in.sin_port = htons(port);
+    addr_in.sin_addr.s_addr = (ip == NULL || strlen(ip) == 0) ? INADDR_ANY : inet_addr(ip);
+    return addr_in;
+}
+
+
+void* sendLoop(SOCKET* socket) {
+    SOCKET client_socket = *socket;
     char client_name[22];
     printf("\nType an alias for this client: ");
     fgets(client_name, sizeof(client_name), stdin);
     client_name[strlen(client_name) - 1] = '\0';
-
-    recvPrintThread(&socketFD);
     char buffer[1024];
     char message[1000];
     while (1) {
@@ -99,15 +106,10 @@ int main() {
             snprintf(buffer, sizeof(buffer), "%s: %s", client_name, message);
             msg_len = strlen(buffer);
         }
-        int result_send = send(socketFD, buffer, msg_len, 0);
+        int result_send = send(client_socket, buffer, msg_len, 0);
         if (result_send == SOCKET_ERROR || strstr(message, "exit()") != NULL)
             break;
     }
-
-    closesocket(socketFD);
-    free(address);
-    WSACleanup();
-    printf("\n");
-    system("pause");
-    return 0;
+    return NULL;
 }
+
